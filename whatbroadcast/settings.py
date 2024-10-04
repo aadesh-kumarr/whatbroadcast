d540679fbd5342dd44afb3ca97f7968d2bf8ab61
Aadesh kumar <aadeshk306@gmail.com>
2024-10-04 12:09:00 +0530

    moulded for vercel

--- whatbroadcast/settings.py
+++ whatbroadcast/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$%^_iza-mr+b#=ghrl4h)fr_pk^wv0gyrje$j!@^ef+5wo&m1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
